82d4c7ebfe0b5dd484742de357f3900cef1c3165
Bulbuc Razvan <37475622+bulbucrazvan@users.noreply.github.com>
2021-10-17 20:32:46 +0300

    Add files via upload

--- Tema1/a.c
+++ Tema1/a.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <strings.h>
-#include <errno.h>
+#include <pthread.h>
 #define KM_PORT 2021
 #define B_PORT 2022
 #define IP "127.0.0.1"
@@ -34,15 +34,23 @@ char* encryptFile(char* encryptionMode, char* cryptKey, char* plainFile) {
     return encryptedFile;
 }
 
+void* getEncryptedKey(void* args) {
+    int socketDescriptor = initializeConnection(IP, KM_PORT);
+    char* receivedKey = (char*)args;
+    receivedKey = readMessage(socketDescriptor);
+    endConnection(socketDescriptor);
+}
+
 int main() {
+    char* receivedKey;
+    pthread_t keyManagerThread;
+    pthread_create(&keyManagerThread, NULL, getEncryptedKey, receivedKey);
     printf("Type encryption mode: ");
     char* encryptionMode = malloc(20);
     scanf("%s", encryptionMode);
-    int socketDescriptor = initializeConnection(IP, KM_PORT);
-    char* receivedKey = readMessage(socketDescriptor);
-    endConnection(socketDescriptor);
-    socketDescriptor = initializeConnection(IP, B_PORT);
+    int socketDescriptor = initializeConnection(IP, B_PORT);
     sendMessage(socketDescriptor, encryptionMode);
+    pthread_join(keyManagerThread, NULL);
     sendMessage(socketDescriptor, receivedKey);
     char* receivedMessage = readMessage(socketDescriptor);
     char* decryptedKey = aes_decrypt(KEY, receivedKey);
